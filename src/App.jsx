3a535b42f7950e504b547a1f701a66e598aae895
Cubby <ananthsraddy@gmail.com>
2026-07-14 17:38:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{
+      textAlign: "center",
+      marginTop: "100px",
+      fontFamily: "Arial"
+    }}>
+      <h1>My First Website</h1>
+
+      <h2>Hi, I'm Cubby 👋</h2>
+
+      <p>I'm learning React and Deployment.</p>
+
+      <button>About Me</button>
+    </div>
+  );
+}
+
+export default App;
